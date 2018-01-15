5fd742f7564a9fdd22bac394dd46e879dc5cb56f
Sadie Staudacher <sadiestaudacher@Sadies-MacBook-Pro.local>
2018-01-14 19:44:42 -0500

    adding problem 3

--- SI364W18_HW1.py
+++ SI364W18_HW1.py
@@ -13,7 +13,7 @@
 # Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class',
 # you see a page that says "Welcome to SI 364!"
 
-from flask import Flask
+from flask import Flask, request
 import requests
 import json
 app = Flask(__name__)
@@ -22,24 +22,6 @@ app.debug = True
 @app.route('/class')
 def hello_to_you():
     return 'Welcome to SI 364!'
-
-@app.route('/movie/<movie_name>')
-def movie_data(movie_name):
-	baseurl = "https://itunes.apple.com/search?"
-	param_dict = {'term': movie_name, 'entity' : 'movie'}
-
-	response = requests.get(baseurl, params = param_dict).json()
-
-
-	return (str(response))
-
-
-
-
-
-if __name__ == '__main__':
-    app.run()
-
 
 ## [PROBLEM 2] - 250 points
 ## Edit the code chunk above again so that if you go to the URL 'http://localhost:5000/movie/<name-of-movie-here-one-word>'
@@ -54,18 +36,45 @@ if __name__ == '__main__':
 #  "results": []
 # }
 
-
 ## You should use the iTunes Search API to get that data.
 ## Docs for that API are here: https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
 ## Of course, you'll also need the requests library and knowledge of how to make a request to a REST API for data.
-
 ## Run the app locally (repeatedly) and try these URLs out!
+
+@app.route('/movie/<movie_name>')
+def movie_data(movie_name):
+	baseurl = "https://itunes.apple.com/search?" #api for itunes
+	param_dict = {'term': movie_name, 'entity' : 'movie'} #setting up the paramaters for the api request
+
+	response = requests.get(baseurl, params = param_dict).json() #making the request with api and params, returning a dictionary
+
+	return (str(response)) #returning a string because html needs a string
+
 
 ## [PROBLEM 3] - 250 points
 
 ## Edit the above Flask application code so that if you run the application locally and got to the URL http://localhost:5000/question, you see a form that asks you to enter your favorite number.
 ## Once you enter a number and submit it to the form, you should then see a web page that says "Double your favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says "Double your favorite number is 4". Careful about types in your Python code!
 ## You can assume a user will always enter a number only.
+@app.route('/question',methods=["GET","POST"])
+def number_form():
+    formstring = """<br><br>
+    <h1>Please enter your favorite number: </h1>
+    <form action="http://localhost:5000/question" method='POST'>
+<input type="text" name="number"> <br><br>
+<input type="submit" value="Enter a whole number"> <br><br>
+""" ## HINT: In there ^ is where you need to add a little bit to the code...
+    if request.method == "POST":
+        result_str = ""
+        for k in request.form:
+            doubledn_num = int(request.form.get(k,"")) ** 2
+            string_of_doubled_num = str(doubledn_num)
+            result_str += "Double your {} is: <i>{}</i><br><br>".format(k, string_of_doubled_num)
+        return formstring + result_str
+        # Add more code here so that when someone enters a phrase, you see their data (somehow) AND the form!
+    else:
+        return formstring
+
 
 
 ## [PROBLEM 4] - 350 points
